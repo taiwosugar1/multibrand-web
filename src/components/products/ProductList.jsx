fcdf28c28b713349d980a87bebde4610c29bbb96
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-14 14:55:46 +0100

    sdw

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -8,7 +8,7 @@ const primaryProducts = [
   { name: 'Tshirt_branding', id: 2, image:"/images/cloth1.png", price:"5000"  },
   { name: 'Bags', id: 3, image:"/images/bag1.png", price:"1000"  },
   { name: 'Branding', id: 4 , image:"/images/brand1.png", price:"" },
-  { name: 'Letterhead', id: 5, image:"", price:""  },
+  { name: 'Letterhead', id: 5, image:"/images/letterhead.png", price:""  },
   { name: 'Custum_Mugs', id: 6, image:"/images/cup1.png", price:""  },
   { name: 'Business_Cards', id: 7, image:"/images/card2.png", price:""  },
   { name: 'Calender', id: 8, image:"/images/calender2.png", price:""  },
