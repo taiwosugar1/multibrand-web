f5c93eb013250e1dde7d1adf3ea5159a4b26cf63
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-14 16:15:39 +0100

    jh

--- src/components/products/Image.js
+++ src/components/products/Image.js
@@ -1,17 +1,17 @@
 
 const images = [
-  { url: 'images/book2.png', title: 'High Quality Printing', description1: 'High-quality printing services for all your business needs.', description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { url: '/images/book2.png', title: 'High Quality Printing', description1: 'High-quality printing services for all your business needs.', description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
 
-  { url: 'images/flyer6.png', title: 'Explore Our Product', description1: 'Affordable prices, exceptional quality, and fast turnaround times.', description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { url: '/images/flyer6.png', title: 'Explore Our Product', description1: 'Affordable prices, exceptional quality, and fast turnaround times.', description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
 
-  { url: 'images/book4.png', title: 'Custom Print Solutions', description1: 'Custom printing solutions to bring your ideas to life.' },
+  { url: '/images/book4.png', title: 'Custom Print Solutions', description1: 'Custom printing solutions to bring your ideas to life.' },
 
-  { url: 'images/book6.png', title: 'Eco-Friendly options', description1: 'Eco-friendly printing options for a greener tomorrow.' },
-  { url: 'images/pen3.png', title: 'Expert Print Services', description1: 'Expert printing services with attention to every detail.' },
-  { url: 'images/book5.png', title: 'Wide Product Range', description1: 'Wide range of products, from business cards to banners.' },
-  { url: 'images/card4.png', title: 'Stand Out Prints', description1: 'Print with us and stand out from the competition.' },
+  { url: '/images/book6.png', title: 'Eco-Friendly options', description1: 'Eco-friendly printing options for a greener tomorrow.' },
+  { url: '/images/pen3.png', title: 'Expert Print Services', description1: 'Expert printing services with attention to every detail.' },
+  { url: '/images/book5.png', title: 'Wide Product Range', description1: 'Wide range of products, from business cards to banners.' },
+  { url: '/images/card4.png', title: 'Stand Out Prints', description1: 'Print with us and stand out from the competition.' },
   { url: '/images/envelope5.png', title: 'Elevate Your Brand', description1: 'Professional printing services to elevate your brand image.'},
-  { url: 'images/label2.png', title: 'Dedicated Customer Support', description1: 'Dedicated customer support to ensure your satisfaction.'},
-  { url: 'images/cloth3.png', title: 'Timely Print Delivery', description1: 'Get your prints delivered on time, every time.'},
+  { url: '/images/label2.png', title: 'Dedicated Customer Support', description1: 'Dedicated customer support to ensure your satisfaction.'},
+  { url: '/images/cloth3.png', title: 'Timely Print Delivery', description1: 'Get your prints delivered on time, every time.'},
 ];
 export default images
