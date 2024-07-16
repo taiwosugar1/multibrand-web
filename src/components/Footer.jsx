07a61a7e495f2f309b5baba4acdb1ca19aed27a2
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-16 23:17:48 +0100

    ddd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 import "./Footer.css"
 import { MdEmail } from 'react-icons/md'
 import { Link } from 'react-router-dom'
+import Contact from '../pages/Contact'
 
 const Footer = () => {
   return (
     <footer className='footer'>
         <div className='footer-one'>
-        <img src="/images/paystack.png" alt="" />
-            <p className='first-image'> Thank you for choosing Multibrand Digital Services, where your ideas take shape and come to life.</p>
+        <Contact/>
         </div>
         
          <div className="footer-social">
@@ -20,9 +20,15 @@ const Footer = () => {
          </div>
 
          <div className="footer-links">
-         <Link to={"/about"}><h4>About Us</h4></Link> 
+         
+             <Link to={"/about"}><h4>About Us</h4></Link> 
          <Link to={"/term-condition"}><h4>T & C</h4></Link> 
          <Link  to={"/privacy-policy"}><h4>Privacy Policy</h4></Link> 
+         
+
+           <img src="/images/paystack.png" alt="" />
+            <p className='first-image'> Thank you for choosing Multibrand Digital Services, where your ideas take shape and come to life.</p>
+
          </div>
          <p className='footer-under'>Copyright 2024 © <b>Multibrand Digital Services</b> <br />All rights reserved.</p>
        </footer>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,20 +31,17 @@ const Contact = () => {
       };
   return (
     <section className='contact-form'>
-        <h1>Contact</h1>
+        <h1>Message Us</h1>
         <form onSubmit={onSubmit}>
             <div className="input-box">
-                <label>Full Name</label>
                 <input type="text" placeholder='Full Name' name='name' required className='field' />
             </div>
 
             <div className="input-box">
-               <label>Email Address</label>
                <input type="email" placeholder='Enter your email' name='email' required className='field' />
             </div>
 
             <div className="input-box">
-               <label>Your Message</label>
                <textarea name="message" placeholder='Enter your message' required className='field message' ></textarea>
             </div>
 
@@ -54,4 +51,7 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
+
+//    <h3>Trust Diamond Visa and Immigration Services for a stress-free application process</h3>
+//           <p>Navigating your path to a brighter future with Diamond Visa and Immigration Services.</p> 
